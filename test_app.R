a401e4bfedc237e5aa8e140b321343cfd2b3b7b7
Billy.Lam <billyla@aswatson.com>
2019-04-23 00:24:12 +0800

    Update UI in test_app

--- test_app.R
+++ test_app.R
@@ -4,60 +4,91 @@ require(shinydashboard)
 library(ggplot2)
 library(dplyr)
 
-recommendation <- read.csv('C:\\Users\\Chun Lam\\Git\\clones\\shiny\\recommendation.csv',stringsAsFactors = F,header=T)
 
+#####################################
+## Setup                           ##
+#####################################
+
+recommendation <- read.csv('C:\\Users\\Chun Lam\\Git\\clones\\shiny\\recommendation.csv',stringsAsFactors = F,header=T)
 head(recommendation)
 
+signal_list <- c('s_bull_stick', 's_bear_stick', 's_bull_engulf', 's_bear_engulf', 's_bull_harami', 's_bear_harami', 's_2day_reverse_good', 's_2day_reverse_bad', 's_bull_pierce', 's_bear_pierce', 's_hammer', 's_shooting_star')
+signal_list_chinese <- c('大陽燭', '大陰燭', '向好吞噬', '向淡吞噬', '向好身懷六甲', '向淡身懷六甲', '向好雙日轉向', '向淡雙日轉向', '曙光初現', '烏雲蓋頂', '鎚頭', '射擊之星')
+signal_radioButton_list <- as.list(setNames(c("none", signal_list), c("None",signal_list_chinese)))
 
-#Dashboard header carrying the title of the dashboard
+
+
+#####################################
+## Build UI                        ##
+#####################################
+
+
 header <- dashboardHeader(title = "Stock analysis")
 
-#Sidebar content of the dashboard
+
 sidebar <- dashboardSidebar(
     sidebarMenu(
-        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-        menuItem("Visit-us", icon = icon("send",lib='glyphicon'),
-                 href = "https://www.salesforce.com")
+        menuItem("Dashboard", tabName = "tab_dashboard", icon = icon("dashboard")),
+        menuItem("Signal"   , tabName = "tab_signal"   , icon = icon("cog", lib = "glyphicon")),
+        menuItem("Stock"    , tabName = "tab_stock"    , icon = icon("bar-chart-o")),
+        menuItem("Option"   , tabName = "tab_option"   , icon = icon("list-alt"))
     )
 )
 
-
-frow1 <- fluidRow(
-    valueBoxOutput("value1"),
-    valueBoxOutput("value2"),
-    valueBoxOutput("value3")
+row_top <- fluidRow(
+    valueBoxOutput("box_success"),
+    valueBoxOutput("box_fail"),
+    valueBoxOutput("box_score")
 )
 
-frow2 <- fluidRow(
+row_bottom <- fluidRow(
 
     box(
-        title = "Revenue per Account"
-        ,status = "primary"
-        ,solidHeader = TRUE
-        ,collapsible = TRUE
-        ,plotOutput("revenuebyPrd", height = "300px")
-    )
+        title = "Control", width = 4, solidHeader = TRUE, status = "primary",
 
-    ,box(
-        title = "Revenue per Product"
-        ,status = "primary"
-        ,solidHeader = TRUE
-        ,collapsible = TRUE
-        ,plotOutput("revenuebyRegion", height = "300px")
+        textInput("dash_input_code", "Stock Number", placeholder = "Stock Number"),
+
+        dateRangeInput("dash_input_date",
+                       "Date range:",
+                       format = "yyyy-mm-dd",
+                       start = "2017-01-01",
+                       end = "2018-12-31",
+                       min = "2010-01-01",
+                       max = "2018-12-31"),
+
+        radioButtons("dash_input_signal",
+                     label    = h3("Signal"),
+                     choices  = signal_radioButton_list,
+                     selected = "none")
+    ),
+
+    box(
+        title       = "Output",
+        status      = "primary",
+        width       = 8,
+        solidHeader = TRUE,
+        collapsible = TRUE,
+        plotOutput("dash_output_graph", height = "600px")
     )
 
 )
 
+row_debug <- fluidRow(
+    box(
+        title = "Debug", width = 8, solidHeader = TRUE, status = "primary",
 
+        verbatimTextOutput("dash_output_debug")
+    )
+)
 
 # combine the two fluid rows to make the body
-body <- dashboardBody(frow1, frow2)
+body <- dashboardBody(row_top, row_bottom, row_debug)
 
 #completing the ui part with dashboardPage
-ui <- dashboardPage(title = 'This is my Page title', header, sidebar, body, skin='red')
+ui <- dashboardPage(title = 'Stock analysis dashboard', header, sidebar, body, skin='red')
 
 # create the server functions for the dashboard
-server <- function(input, output) {
+server <- function(input, output, session) {
 
     #some data manipulation to derive the values of KPI boxes
     total.revenue <- sum(recommendation$Revenue)
@@ -66,37 +97,31 @@ server <- function(input, output) {
 
 
     #creating the valueBoxOutput content
-    output$value1 <- renderValueBox({
+    output$box_success <- renderValueBox({
         valueBox(
-            formatC(sales.account$value, format="d", big.mark=',')
-            ,paste('Top Account:',sales.account$Account)
-            ,icon = icon("stats",lib='glyphicon')
-            ,color = "purple")
+            formatC(23, format="d", big.mark=','),
+            "Success",
+            icon = icon("thumbs-up"),
+            color = "green")
+    })
 
+    output$box_fail <- renderValueBox({
+
+        valueBox(
+            formatC(12, format="d", big.mark=','),
+            'Fail',
+            icon = icon("thumbs-down"),
+            color = "red")
 
     })
 
-
-
-    output$value2 <- renderValueBox({
+    output$box_score <- renderValueBox({
 
         valueBox(
-            formatC(total.revenue, format="d", big.mark=',')
-            ,'Total Expected Revenue'
-            ,icon = icon("gbp",lib='glyphicon')
-            ,color = "green")
-
-    })
-
-
-
-    output$value3 <- renderValueBox({
-
-        valueBox(
-            formatC(prof.prod$value, format="d", big.mark=',')
-            ,paste('Top Product:',prof.prod$Product)
-            ,icon = icon("menu-hamburger",lib='glyphicon')
-            ,color = "yellow")
+            formatC(123, format="d", big.mark=','),
+            "Signal Score",
+            icon = icon("star"),
+            color = "yellow")
 
     })
 
